c0e742c90b448a801369224d3f6b9f6525239bfe
Jasurbek <jasurdev1604@gmail.com>
2026-09-02 04:11:20 +0500

    Refactor window functions SQL script: enhance queries with additional aggregations, rankings, and partitioning for improved data analysis

--- sql-server/13-window-functions.sql
+++ sql-server/13-window-functions.sql
@@ -1,13 +1,106 @@
-SELECT SUM(Sales)
-FROM   Sales.Orders;
-
-SELECT   ProductID,
-         SUM(Sales)
+SELECT   OrderID,
+         OrderDate,
+         ProductID,
+         SUM(Sales) AS TotalSales
 FROM     Sales.Orders
-GROUP BY ProductID;
+GROUP BY ProductID, OrderID, OrderDate;
 
-SELECT ProductID,
+SELECT OrderID,
        OrderDate,
-       OrderID,
-       SUM(Sales) OVER (PARTITION BY ProductID)
+       ProductID,
+       SUM(Sales) OVER (PARTITION BY ProductID) AS TotalSalsByProductID
 FROM   Sales.Orders;
+
+SELECT OrderID,
+       OrderDate,
+       ProductID,
+       Sales,
+       OrderStatus,
+       SUM(Sales) OVER () AS TotalSales,
+       SUM(Sales) OVER (PARTITION BY ProductID) AS TotalSalsByProductID,
+       SUM(Sales) OVER (PARTITION BY ProductID, OrderStatus) AS TotalSalsByProductIDandOrderStatus
+FROM   Sales.Orders;
+
+SELECT OrderID,
+       OrderDate,
+       Sales,
+       RANK() OVER (ORDER BY Sales DESC) AS RankSales
+FROM   Sales.Orders;
+
+SELECT OrderID,
+       OrderDate,
+       OrderStatus,
+       Sales,
+       SUM(Sales) OVER (PARTITION BY OrderStatus ORDER BY OrderDate ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING)
+FROM   Sales.Orders;
+
+SELECT OrderID,
+       OrderDate,
+       CustomerID,
+       COUNT(*) OVER () AS TotalOrders,
+       COUNT(*) OVER (PARTITION BY CustomerID) AS OrdersByCustomers
+FROM   Sales.Orders;
+
+SELECT CustomerID,
+       FirstName,
+       LastName,
+       Country,
+       Score,
+       COUNT(*) OVER () AS TotalCustomers,
+       COUNT(Score) OVER () AS TotaScoreCustomers
+FROM   Sales.Customers;
+
+SELECT OrderID,
+       COUNT(*) OVER (PARTITION BY OrderID) AS CheckPK
+FROM   Sales.OrdersArchive;
+
+SELECT OrderId,
+       OrderDate,
+       Sales,
+       ProductID,
+       SUM(Sales) OVER () AS total_sales,
+       SUM(Sales) OVER (PARTITION BY ProductID) AS each_product_sales
+FROM   Sales.Orders;
+
+SELECT OrderID,
+       ProductID,
+       Sales,
+       SUM(Sales) OVER (),
+       ROUND((CAST (Sales AS FLOAT) / SUM(Sales) OVER ()) * 100, 2)
+FROM   Sales.Orders;
+
+SELECT OrderID,
+       OrderDate,
+       AVG(Sales) OVER () AS AvarageSales,
+       AVG(Sales) OVER (PARTITION BY ProductID) AS AvarageSalesByProduct
+FROM   Sales.Orders;
+
+SELECT CustomerID,
+       LastName,
+       Score,
+       AVG(Score) OVER ()
+FROM   Sales.Customers;
+
+SELECT *
+FROM   (SELECT OrderID,
+               ProductID,
+               Sales,
+               AVG(Sales) OVER () AS AvgSales
+        FROM   Sales.Orders) AS t
+WHERE  Sales > AvgSales;
+
+SELECT OrderID,
+       OrderDate,
+       Sales,
+       ProductID,
+       MIN(Sales) OVER () AS min_sales,
+       MAX(Sales) OVER () AS max_sales,
+       MIN(Sales) OVER (PARTITION BY ProductID) AS min_sales_by_product,
+       MAX(Sales) OVER (PARTITION BY ProductID) AS max_sales_by_product
+FROM   Sales.Orders;
+
+SELECT *
+FROM   (SELECT *,
+               MAX(Salary) OVER () AS HighestSalary
+        FROM   Sales.Employees) AS t
+WHERE  Salary = HighestSalary;
