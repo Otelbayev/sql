2d493eb0d58a63694186b9a7ba88f1c432679706
Jasurbek <jasurdev1604@gmail.com>
2026-09-24 01:05:10 +0500

    Add detailed comments and create indexes for customer queries

--- sql-server/20-indexes.sql
+++ sql-server/20-indexes.sql
@@ -1,1 +1,38 @@
 --INDEXES
+-- data pate
+-- index page
+-- heap
+-- clustered index
+-- b-tree
+-- non clustered index
+--rowstore index
+--columnstore index
+
+SELECT *
+FROM   Sales.DBCustomers
+WHERE  CustomerID = 1;
+
+CREATE CLUSTERED INDEX idx_DBCustomers_CustomerID
+    ON Sales.DBCustomers(CustomerID);
+
+DROP INDEX idx_DBCustomers_CustomerID
+    ON Sales.DBCustomers;
+
+-------------------------------------------
+SELECT *
+FROM   Sales.DBCustomers
+WHERE  FirstName = 'Anna';
+
+CREATE INDEX idx_DBCustomers_FirstName
+    ON Sales.DBCustomers(FirstName);
+
+-------------------------------------------
+SELECT *
+FROM   Sales.DBCustomers
+WHERE  Country = 'USA'
+       AND Score > 500;
+
+CREATE INDEX idx_DBCustomers_CountryScore
+    ON Sales.DBCustomers(Country, Score);
+
+    
